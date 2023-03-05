2143e6bfffab774ae54cde1d0b1157dd48489865
Sarel <sarelmicha93@gmail.com>
2023-03-05 22:48:25 +0200

    no message

--- Rings-Quest-Client/Assets/Scripts/Gameplay/Playable/Token/TapToken.cs
+++ Rings-Quest-Client/Assets/Scripts/Gameplay/Playable/Token/TapToken.cs
@@ -14,6 +14,7 @@ namespace Happyflow.RingsQuest.Gameplay.Playable.Token
 
         protected override void OnTokenClick()
         {
+            Debug.Log("im here!!");
             Smash();
         }
     }
